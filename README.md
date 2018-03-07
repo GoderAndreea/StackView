f9ddc474200d34ec070454c48c32d8080430b68f
GoderAndreea <goderandreea@gmail.com>
2018-03-07 21:18:41 +0200

    first commit
    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StackView
+In this project I've learn how to use stack.
+
---
+++ stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stack
+//
+//  Created by Costin Valu on 3/6/18.
+//  Copyright © 2018 Andreea Goder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
